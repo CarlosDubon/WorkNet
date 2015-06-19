31d7f5d1b52fdc6991429481938a1eb4c43a7384
Julio César Rodríguez Gómez <junior.rod78@gmail.com>
2015-06-19 15:47:24 -0600

    validaciones

---
+++ vistas/menuReportes.php
@@ -0,0 +1,18 @@
+<div class="panel panel-info">
+  <div class="panel-heading">
+    <h3 class="panel-title">Reportes</h3>
+  </div>
+  <div class="panel-body">
+    <div class="panel panel-default" id="RpUser">
+         <div class="panel-body">
+             <label><h6>Reporte de Usuarios</h6></label><br>
+             <a href="#" class="btn btn-default">Generar</a>
+        </div>
+  </div>
+    <div class="panel panel-default" id="RpUserc">
+         <div class="panel-body">
+             <label><h6>Reporte de Eventos</h6></label><br>
+             <a href="#" class="btn btn-info">Generar</a>
+        </div>
+  </div>
+</div>
