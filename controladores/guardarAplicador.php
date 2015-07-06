9e330a765680dd5082edcee5c19e6687042d99e4
CarlosDubon <carlosminerodubon@gmail.com>
2015-07-06 17:49:31 -0600

    permisos de usuarios

--- controladores/guardarAplicador.php
+++ controladores/guardarAplicador.php
@@ -1,5 +1,10 @@
 <?php
 require_once realpath(dirname(__FILE__) . '/../clases/Aplicadores.php');
+require_once realpath(dirname(__FILE__) . '/../clases/AdministrarUsuarios.php');
+
+$admin = new AdministrarUsuarios();
+$usuariosPermitidos = array('4');
+$admin ->verificarSesion($usuariosPermitidos);
 
 $app = new Aplicadores();
 $a = $_POST;
