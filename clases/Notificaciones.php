3bcfcaec8d3eabe460ba7ca7a4c13aa2e343c35f
CarlosDubon <carlosminerodubon@gmail.com>
2015-07-09 20:08:00 -0600

    clases actualizadas sin traducir

--- clases/Notificaciones.php
+++ clases/Notificaciones.php
@@ -14,8 +14,46 @@ class Notificaciones{
         $query='SELECT new_count FROM cuenta WHERE idCuenta = '.$id;
         $resultado = $bd->consulta($query);
         
+        $query2='SELECT idSoli as id,Fecha,Tipo,cuentaAmigo,cuenta_idCuenta,user_idCuenta FROM solicitudes WHERE cuentaAmigo='.$id;
+        $resultado2 = $bd->consulta($query2);
+        
+        $variables['user']= $resultado2[0]['user_idCuenta'];
+        $variables['id']= $resultado2[0]['cuenta_idCuenta'];
+        $variables['listaNew']= $this->convertirNotiHTML($resultado2);
         $variables['news']=$resultado[0]['new_count'];
         
         $plantilla->verPagina('verNotificaciones', $variables);
     }
+    
+    
+    public function convertirNotiHTML($Noti = array()){
+        $bd = new MySQL();
+        $sesion = new Sesion();
+        
+        $idUser = $sesion->obtenerVariableSesion('idUsuario');
+        
+        $noti = '';
+        
+       for($i=0; $i < count($Noti); $i++ ){
+        
+       switch ($Noti[$i]['Tipo']){
+            case 1:
+
+                $noti.='<div class="alert alert-dismissible alert-info">
+                          <button type="button" class="close" data-dismiss="alert">×</button>
+                          The user <strong>'.$Noti[$i]['user_idCuenta'].'</strong>  is following you. <a href="./agregarAmigo.php?idCuenta='.$Noti[0]['cuenta_idCuenta'].'" class="alert-link">Follow back</a> <strong id="de">'.substr($Noti[0]['Fecha'],0,-9).'</strong>
+                        </div>';
+            break;
+            case 2:
+                $noti.='<div class="alert alert-dismissible alert-danger">
+                          <button type="button" class="close" data-dismiss="alert">×</button>
+                          The user <strong>'.$Noti[$i]['user_idCuenta'].'</strong>  created an event. <a href="../vistas/calendarioEmpresa.php" class="alert-link">See Event</a> <strong id="de">'.substr($Noti[0]['Fecha'],0,-9).'</strong>
+                        </div>';
+            break;
+            }
+        }
+         return $noti;
+
+    }
+    
 }
