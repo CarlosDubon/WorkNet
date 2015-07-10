8819e1dad9c195d0a11a21b2d066cb71efa258ab
CarlosDubon <carlosminerodubon@gmail.com>
2015-07-09 20:12:44 -0600

    fragmentos actualizados

--- vistas/fragmento/NavbarL.php
+++ vistas/fragmento/NavbarL.php
@@ -1,5 +1,4 @@
 <a href="./mensajes.php" class="list-group-item"><span class="fui-chat"></span>Amigos</a>
-<a href="#" class="list-group-item"><span class="fui-video"></span>Videoconferencia</a>
 <a href="./creartrabajador.php" class="list-group-item"><span class="fui-plus"></span>Crear un trabajador</a>
 <a href="./usuarios_empre.php" class="list-group-item"><span class="fui-user"></span>Cuentas</a>
 <a href="#" class="list-group-item"><span class="fui-calendar-solid"></span>Eventos</a>
